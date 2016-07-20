eeee781882c83541c8a9222bb8f32f93276461f3
Oscar Anton <oscar@molecularts.com>
2016-07-20 17:24:13 +0200

    Added Framework Target for Carthage

---
+++ PullToBounceFramework/PullToBounceFramework.h
@@ -0,0 +1,19 @@
+//
+//  PullToBounceFramework.h
+//  PullToBounceFramework
+//
+//  Created by Oscar Anton on 20/7/16.
+//  Copyright © 2016 Uniface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PullToBounceFramework.
+FOUNDATION_EXPORT double PullToBounceFrameworkVersionNumber;
+
+//! Project version string for PullToBounceFramework.
+FOUNDATION_EXPORT const unsigned char PullToBounceFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PullToBounceFramework/PublicHeader.h>
+
+
